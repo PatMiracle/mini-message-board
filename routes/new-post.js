11b0fddd4761bc22465d9d364e254328adf46a81
Miracle <miraclepatidehen@gmail.com>
2024-03-22 18:35:41 +0100

    update: error rendering

--- routes/new-post.js
+++ routes/new-post.js
@@ -4,13 +4,15 @@ const router = express.Router();
 const messages = require("../data");
 
 router.get("/", (req, res) => {
-  res.status(200).render("new-post");
+  res.status(200).render("new-post", { error: false });
 });
 
 router.post("/", (req, res) => {
   const { name, message } = req.body;
   if (!name || !message) {
-    return res.status(400).send("please provide required fields");
+    return res
+      .status(400)
+      .render("new-post", { error: "please fill required fields" });
   }
   messages.push({ name: name, message: message, time: new Date() });
   res.redirect("/");
